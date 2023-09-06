ba4a6cda6f6870f2090e158904ed49677f70b877
MM91 <mmv_91@bk.ru>
2023-09-06 18:27:04 +0500

    style : удалить неиспользуемый импорт и не нужные комментарии.

--- src/main/java/ru/yandex/practicum/filmorate/validators/exceptions/ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.validators.exceptions;
-
-public class ObjectNotFoundException extends RuntimeException{
-    public ObjectNotFoundException(String message) {
-        super(message);
-    }
-}
