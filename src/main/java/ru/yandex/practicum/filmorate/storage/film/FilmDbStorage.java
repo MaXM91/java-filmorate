790832a4581b6b4d8810e280ee28b9427ab73bd0
MM91 <mmv_91@bk.ru>
2023-09-07 05:03:21 +0500

    style : править оформление

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -32,8 +32,8 @@ public class FilmDbStorage implements FilmStorage {
 
         jdbcTemplate.update(connection -> {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT INTO films (name," + " description, releaseDate, duration) values (?, ?, ?, ?)",
-                new String[]{"id"});
+                "INSERT INTO films (name," +
+                    " description, releaseDate, duration) values (?, ?, ?, ?)", new String[]{"id"});
             preparedStatement.setString(1, film.getName());
             preparedStatement.setString(2, film.getDescription());
             preparedStatement.setDate(3, Date.valueOf(film.getReleaseDate()));
@@ -58,10 +58,9 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public Film update(Film film) {
 
-        jdbcTemplate.update(
-            "UPDATE films SET name = ?, description = ?, releasedate = ?," + " duration = ? WHERE id = ?",
-            film.getName(), film.getDescription(), Date.valueOf(film.getReleaseDate()), film.getDuration(),
-            film.getId());
+        jdbcTemplate.update("UPDATE films SET name = ?, description = ?, releasedate = ?," +
+                " duration = ? WHERE id = ?", film.getName(), film.getDescription(),
+            Date.valueOf(film.getReleaseDate()), film.getDuration(), film.getId());
 
         if (film.getGenres() != null) {
             jdbcTemplate.update("DELETE FROM film_genre WHERE film_id = ?", film.getId());
@@ -93,10 +92,10 @@ public class FilmDbStorage implements FilmStorage {
     public Film found(Integer id) {
         try {
             return jdbcTemplate.queryForObject("SELECT * FROM films WHERE id = ?",
-                (rs, rowNum) -> new Film(rs.getInt("id"), rs.getString("name"), rs.getString("description"),
-                    readFilmGenre(rs.getInt("id")), readFilmMpa(rs.getInt("id")), readUsersLike(id),
-                    rs.getDate("releaseDate")
-                        .toLocalDate(), rs.getInt("duration")), id);
+                (rs, rowNum) -> new Film(rs.getInt("id"), rs.getString("name"),
+                    rs.getString("description"), readFilmGenre(rs.getInt("id")),
+                    readFilmMpa(rs.getInt("id")), readUsersLike(id), rs.getDate("releaseDate")
+                    .toLocalDate(), rs.getInt("duration")), id);
         } catch (RuntimeException exc) {
             throw new ObjectNotFoundException("FilmDbStorage/found: film not found!");
         }
@@ -106,8 +105,9 @@ public class FilmDbStorage implements FilmStorage {
     public List<Film> get() {
 
         return jdbcTemplate.query("SELECT * FROM films",
-            (rs, rowNum) -> new Film(rs.getInt("id"), rs.getString("name"), rs.getString("description"),
-                readFilmGenre(rs.getInt("id")), readFilmMpa(rs.getInt("id")), readUsersLike(rs.getInt("id")),
+            (rs, rowNum) -> new Film(rs.getInt("id"), rs.getString("name"),
+                rs.getString("description"), readFilmGenre(rs.getInt("id")),
+                readFilmMpa(rs.getInt("id")), readUsersLike(rs.getInt("id")),
                 rs.getDate("releaseDate")
                     .toLocalDate(), rs.getInt("duration")));
     }
@@ -117,13 +117,14 @@ public class FilmDbStorage implements FilmStorage {
         try {
 
             return jdbcTemplate.query(
-                "SELECT f.id, f.name, f.description, f.releasedate, f.duration, COUNT(l.user_id) AS rate\n" +
-                    "FROM films AS f\n" + "LEFT JOIN likes AS l ON f.ID = l.film_id\n" + "GROUP BY f.id\n" +
-                    "ORDER BY rate DESC\n" + "LIMIT ? ",
-                (rs, rowNum) -> new Film(rs.getInt("id"), rs.getString("name"), rs.getString("description"),
-                    readFilmGenre(rs.getInt("id")), readFilmMpa(rs.getInt("id")), rs.getInt("rate"),
-                    rs.getDate("releaseDate")
-                        .toLocalDate(), rs.getInt("duration")), (int) count);
+                "SELECT f.id, f.name, f.description, f.releasedate, f.duration, COUNT(l.user_id) " +
+                    "AS rate\n" +
+                    "FROM films AS f\n" + "LEFT JOIN likes AS l ON f.ID = l.film_id\n" +
+                    "GROUP BY f.id\n" + "ORDER BY rate DESC\n" + "LIMIT ? ",
+                (rs, rowNum) -> new Film(rs.getInt("id"), rs.getString("name"),
+                    rs.getString("description"), readFilmGenre(rs.getInt("id")),
+                    readFilmMpa(rs.getInt("id")), rs.getInt("rate"), rs.getDate("releaseDate")
+                    .toLocalDate(), rs.getInt("duration")), (int) count);
 
         } catch (EmptyResultDataAccessException e) {
             return null;
@@ -140,11 +141,12 @@ public class FilmDbStorage implements FilmStorage {
             } else {                                       //
                 changeRepeat.add(genre.getId());           //
             }                                              //
-            jdbcTemplate.update("INSERT INTO film_genre (film_id, genre_id) VALUES(?,?)", film.getId(),
-                genre.getId()); // Ввести пару
+            jdbcTemplate.update("INSERT INTO film_genre (film_id, genre_id) VALUES(?,?)",
+                film.getId(), genre.getId()); // Ввести пару
 
             Genre fullGenre = jdbcTemplate.queryForObject("SELECT * FROM genre WHERE genre_id = ?",
-                (rs, rowNum) -> new Genre(rs.getInt("genre_id"), rs.getString("name")), genre.getId());
+                (rs, rowNum) -> new Genre(rs.getInt("genre_id"), rs.getString("name")),
+                genre.getId());
 
             newList.add(fullGenre);
         }
@@ -154,10 +156,11 @@ public class FilmDbStorage implements FilmStorage {
 
     private List<Genre> readFilmGenre(Integer id) {
         try {
-            List<Genre> genres = jdbcTemplate.query("SELECT fg.genre_id, g.name \n" + "FROM films AS f\n" +
+            List<Genre> genres = jdbcTemplate.query(
+                "SELECT fg.genre_id, g.name \n" + "FROM films AS f\n" +
                     "LEFT JOIN film_genre AS fg ON f.id=fg.film_id \n" +
-                    "LEFT JOIN genre AS g ON fg.genre_id=g.genre_id\n" + "WHERE f.id = ?\n" + "GROUP BY g.genre_id",
-                (rs, rowNum) -> {
+                    "LEFT JOIN genre AS g ON fg.genre_id=g.genre_id\n" + "WHERE f.id = ?\n" +
+                    "GROUP BY g.genre_id", (rs, rowNum) -> {
                     if (rs.getInt("genre_id") == 0) {
                         return null;
                     } else {
@@ -176,8 +179,9 @@ public class FilmDbStorage implements FilmStorage {
     }
 
     private Mpa changeFilmMpa(Film film) {
-        jdbcTemplate.update("INSERT INTO film_mpa (film_id, mpa_id) VALUES(?, ?)", film.getId(), film.getMpa()
-            .getId());
+        jdbcTemplate.update("INSERT INTO film_mpa (film_id, mpa_id) VALUES(?, ?)", film.getId(),
+            film.getMpa()
+                .getId());
 
         return jdbcTemplate.queryForObject("SELECT * FROM mpa WHERE mpa_id = ?",
             (rs, rowNum) -> new Mpa(rs.getInt("Mpa_id"), rs.getString("name")), film.getMpa()
@@ -186,9 +190,10 @@ public class FilmDbStorage implements FilmStorage {
 
     private Mpa readFilmMpa(Integer id) {
         try {
-            return jdbcTemplate.queryForObject(
-                "SELECT fm.mpa_id, m.name \n" + "FROM films AS f\n" + "LEFT JOIN film_mpa AS fm ON f.id=fm.film_id \n" +
-                    "LEFT JOIN mpa AS m ON fm.mpa_id=m.mpa_id\n" + "WHERE f.id = ?\n" + "GROUP BY fm.mpa_id, m.name",
+            return jdbcTemplate.queryForObject("SELECT fm.mpa_id, m.name \n" + "FROM films AS f\n" +
+                    "LEFT JOIN film_mpa AS fm ON f.id=fm.film_id \n" +
+                    "LEFT JOIN mpa AS m ON fm.mpa_id=m.mpa_id\n" + "WHERE f.id = ?\n" +
+                    "GROUP BY fm.mpa_id, m.name",
                 (rs, rowNum) -> new Mpa(rs.getInt("mpa_id"), rs.getString("name")), id);
 
         } catch (EmptyResultDataAccessException e) {
@@ -198,7 +203,8 @@ public class FilmDbStorage implements FilmStorage {
 
     private Integer readUsersLike(Integer filmId) {
         try {
-            return jdbcTemplate.queryForObject("SELECT COUNT(user_id) AS c FROM likes WHERE film_id = ?",
+            return jdbcTemplate.queryForObject(
+                "SELECT COUNT(user_id) AS c FROM likes WHERE film_id = ?",
                 (rs, rowNum) -> (rs.getInt("c")), filmId);
         } catch (EmptyResultDataAccessException e) {
             return null;
